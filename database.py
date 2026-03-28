f6055f2a1e891805aee26a6ad5db2b1ef4779fd7
BorisTheJacket <skyfish100@gmail.com>
2026-03-29 00:56:17 +0700

    update to local server

--- database.py
+++ database.py
@@ -1,22 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./habit_friends.db")
+# SQLite database
+SQLALCHEMY_DATABASE_URL = "sqlite:///./habit_friends.db"
 
-# Force IPv4 connection for psycopg2
-if "postgresql" in SQLALCHEMY_DATABASE_URL:
-    SQLALCHEMY_DATABASE_URL += "&application_name=habit_friends"
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL,
-        connect_args={
-            "keepalives": 1,
-            "keepalives_idle": 30,
-        }
-    )
-else:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
