6ba66aa3dbc98c95f5bf31c813e780a2d538f844
BorisTheJacket <skyfish100@gmail.com>
2026-04-09 22:25:14 +0700

    Fix: username not unique

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
-    username = Column(String, unique=True, index=True)
+    username = Column(String, unique=False, index=True)
     email = Column(String, unique=True, index=True)
     avatar = Column(String, nullable=True)
     hashed_password = Column(String)
